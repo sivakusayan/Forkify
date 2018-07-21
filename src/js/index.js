688b5a59d8f81776a76c8f101b5de405b75b2af9
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-07-21 13:18:33 -0400

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import number from './test';
+const x = 25;
+
+console.log(`${number} was imported and our variable x is ${x}`);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill','./src/js/index.js'], 
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
